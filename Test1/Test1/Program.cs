4f73b2a68d4cfe58c46b0074444e4973876f9041
JoaoVitorBarboza <jvbarbosa41298@gmail.com>
2022-10-25 19:42:47 -0300

    testes
    teste do pedra papel e tesoura

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                    Console.Write("Enter ROCK, PAPER OR SCISSORS:");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                    Console.WriteLine();
+                }
+            }
+
+
+            //Console.ReadKey();{
+        }
+
+
+    }
